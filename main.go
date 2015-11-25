17e34af89fcff0375e5dadc15e7c8f9c9c246df8
Hans Meyer <hamster.of.dev@gmail.com>
2015-11-25 21:16:53 +0100

    :rocket: basic gin integration and a running webserver

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 func main() {
-	return 
+	router := gin.Default()
+	
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong");
+	})
+	router.Run(":5000")
+	
+	return
 }
