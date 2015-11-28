7523309a91ac971100e1295f4a631dc5ee07660f
Hans Meyer <hamster.of.dev@gmail.com>
2015-11-28 10:12:58 +0100

    :triumph: Dependecy versioning with godep

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -4,6 +4,9 @@ set -e
 
 # Setup script that makes sure we have everything installed that is needed
 
+# change to the repository root, run go and its tools from there
+cd "$(dirname "$0")/.."
+
 echo "=> Checking go Environment..."
 
 if [! -e $GOPATH] then
@@ -12,3 +15,8 @@ echo "==> Seems like Go is not installed. Please download and setup Go for your"
 echo "    platform"
 
 fi
+export PATH=$PATH:$GOPATH/bin
+
+echo "=> Restoring dependencies with Godep..."
+go get -u github.com/tools/godep
+godep restore
